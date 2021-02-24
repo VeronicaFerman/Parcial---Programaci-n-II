618ab8e5f5a4f49c64ffe6df20e39f9b7f08e4a1
Veronica Ferman <veronica.fermanperez>
2021-02-23 20:32:52 -0600

    Pregunta 3 sin session storage

---
+++ pregunta3/script.js
@@ -0,0 +1,23 @@
+function guardar(){
+   
+    var _fech = document.getElementById("fech").value;
+    var _tipo = document.getElementById("tipo").value;
+    var _vaso = document.getElementById("vaso").value;
+
+    if(_vaso < 8){
+        var _msm = "Tomar más agua"
+    } else{
+        var _msm = "Meta alcanzada"
+    }
+
+    var fila="<tr><td>"+_fech+"</td><td>"+_tipo +"</td><td>"+_vaso +"</td></tr>"+_msm +"</td></tr>";
+
+    var btn = document.createElement("TR");
+   	btn.innerHTML=fila;
+    document.getElementById("tablita").appendChild(btn);
+    document.tabla.appendChild(fila)
+}
+
+function writeElementToSessionStorage(keyName, arrayObject) {
+    sessionStorage.setItem(keyName, JSON.stringify(arrayObject))
+}
